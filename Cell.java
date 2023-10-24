00d8865d255690ea8e58843db8fd1d996df833a4
thea2806 <147829542+thea2806@users.noreply.github.com>
2023-10-24 19:36:52 +0200

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -9,20 +9,32 @@ public class Cell {
     boolean hasAbility;
     JButton tile = new JButton();
 
-    public Cell(int row, int column) {
-        //MinesweeperGUI.Tiles button = new MinesweeperGUI.Tiles(row, column);
+    public Cell(int row, int column) {        
         this.row = row;
         this.column = column;
         this.hasMine = false;
         revealed = false;
         hasAbility = false;
-       // this.tile = tiles;
     }
 
+    public void addButton() {
+        MinesweeperGUI board = new MinesweeperGUI(row,column); //row inseamna rowSize
+        JButton button = new JButton();
+        button = board.getButton(row, column);
+        this.tile = button;
+    }
+
+    public JButton getButton() {
+        return this.tile;
+    }
 
 
     public void setNeighborMines(int neighborMines) {
         this.neighborMines = neighborMines;
+    }
+
+    public void setRevealed() {
+        this.revealed = true;
     }
 
 }
